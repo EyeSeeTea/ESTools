e00b839d53c4ffa8df167003574740e07a72659c
Arnau Sanchez <pyarnau@gmail.com>
2017-05-25 11:59:42 +0200

    Global refactor of dhis2-installer

--- DHIS2/installer/README.md
+++ DHIS2/installer/README.md
@@ -1,32 +1,53 @@
 # dhis2-installer
 
-Shell script to install DHIS2 instances (database and tomcat WAR)
+Shell script to update existing DHIS2 instances (database and, optionally on a hard update, the WAR file)
 
 ## Setup
 
 ```
-install -m 775 dhis2_installer.sh /usr/local/bin/dhis2-installer
-cp dhis2_installer.conf.example $HOME/.dhis2_installer.conf
+$ sudo install -m 775 dhis2_installer.sh /usr/local/bin/dhis2-installer
+$ cp dhis2-installer.conf.example $HOME/.dhis2_installer.conf
 ```
 
-## Example
+Now customize `$HOME/.dhis2_installer.conf` with your particular setup.
 
-### dhis2-update update
+## Examples
 
-* Update a dhis using a profile:
+### Command <update>
+
+
+* Update a DHIS2 instance using the profile _current_:
 
 ```
 $ dhis2-installer update current
 ```
 
-* Update a dhis using a profile with custom options:
+* Update a DHIS2 instance using the profile _previous1_ with extra/overriding options:
 
 ```
-$ dhis2-installer update current --hard
+$ dhis2-installer update previous1 --hard
 ```
 
-### dhis2-update run-analytics
+### Command <run-analytics>
 
 ```
-$ dhis2-installer run-analytics --server-url="https://admin:district@play.dhis2.org/demo/"
+$ dhis2-installer run-analytics --server-url="https://admin:district@play.dhis2.org/demo"
+```
+
+## Crontab
+
+You may want to run the scripts periodically. Use your user crontab:
+
+```
+$ crontab -e
+```
+
+For example, this will run all 4 profiles on different times to avoid hogging the machine:
+
+```
+$ crontab -l
+00 03  * * * /usr/local/bin/dhis2-installer update dev
+30 03  * * * /usr/local/bin/dhis2-installer update current
+00 04  * * * /usr/local/bin/dhis2-installer update previous1
+30 04  * * * /usr/local/bin/dhis2-installer update previous2
 ```
---
+++ DHIS2/installer/dhis2_installer.sh
@@ -0,0 +1,352 @@
+#!/bin/bash
+set -e -u -o pipefail
+
+# Bash set options:
+#   -e          -- Exit when any command fails
+#   -u          -- Exit when the script uses an undefined variable
+#   -o pipefail -- Exit when a command in a pipe fails
+#   -o posix    -- Enable Posix mode (this way, commands in subshells will terminate the script)
+
+# Write strings to stderr
+stderr() { echo -e "$@" >&2; }
+
+# Output strings to sderr with date prefix
+debug() { stderr "[$(date +%Y-%m-%d_%H-%M-%S)] $@"; }
+
+# Write strings to stderr and exit with status code 1
+die() { debug "$@" && exit 1; }
+
+# Remove leading and trailing spaces of $1
+trim() { sed -e 's/^[[:space:]]*//; s/[[:space:]]*$//' <<< "$1"; }
+
+# Set key/values in global array from a INI-style configuration file
+parse_ini_section() { local global_array=$1 file=$2 section=$3
+  local section_contents
+  section_contents=$(
+    awk "/^\s*\[$section\]/ { flag=1; next} /^\s*\[/ { flag=0 } flag" "$file" |
+    grep -v "^[[:space:]]$" | grep -v "^[[:space:]]#"
+  ) || {
+    debug "Cannot read section [$section] from $file"
+    return 1
+  }
+  while IFS="=" read key value; do
+    eval "$global_array[$(trim "$key")]=$(trim "$value")"
+  done <<< "$section_contents"
+}
+
+# Parse arguments and populatie a global array
+#   $1 - Global array variable to populate
+#   $2 - Coded options. Example: "output-file:|-o verbose|b no-verify"
+#   $3 - Function name that prints the help message
+parse_args() { local global_array=$1 coded_options=$2 print_help=$3
+  local short_options long_options params value option_name short_option_name short long arg
+  declare -A args_short args_long args_short_to_option_name args_value
+
+  for option in $coded_options; do
+    IFS="|" read long short <<< "$option"
+    value=$([[ "${option}" =~ :$ ]] && echo "yes" || echo "no")
+    option_name=${long/%:/}
+    short_option_name=${short/%:/}
+    args_value[$option_name]=$value
+    args_long[$long]=1
+    test "$short" && args_short_to_option_name[$short_option_name]=$option_name
+    test "$short" && args_short[$short]=$long
+  done
+
+  short_options=$(echo "${!args_short[*]}" | tr ' ' ',')
+  long_options=$(echo "${!args_long[*]}" | tr ' ' ',')
+  params=$(getopt -o "h$short_options" -l "help,$long_options" --name "$0" -- "$@")
+  eval set -- "$params"
+
+  while true; do
+    arg=$1
+    option_name=$(sed "s/^-*//" <<< "$arg")
+    if test "${arg:0:1}" == "-" && test "${arg:0:2}" != "--"; then # short option
+      option_name=${args_short_to_option_name[${option_name:-_}]:-_}
+    fi
+    value=${args_value[${option_name:-_}]:-not_present}
+
+    if test "$arg" == "--"; then
+      break;
+    elif test "$arg" == "-h" || test "$arg" == "--help"; then
+      $print_help
+      exit 0
+    elif test "$value" == "not_present"; then
+      stderr "Unknown option: $arg"
+      return 1
+    elif test "$value" == "yes"; then
+      eval "$global_array[$option_name]=$2"
+      shift 2
+    elif test "$value" == "no"; then
+      eval "$global_array[$option_name]=yes"
+      shift 1
+    else
+      die "Parsing error: arg=$arg, option_name=$option_name, value=$value"
+    fi
+  done
+}
+
+### App
+
+# Global variables
+# args : ([option-name-with-value]=VALUE [option-flag]=yes)
+declare -g -A args
+# profile used
+declare -g profile
+
+download() { local destdir=$1 url=$2
+  local filename
+  filename="$destdir/$(basename "$url")"
+  debug "Download: $url"
+  if wget -q -O "$filename" "$url"; then
+    echo $filename
+  else
+    debug "Could not download: $url"
+    return 1
+  fi
+}
+
+start_dhis_server() { local command=$1
+  debug "Start DHIS2 server: $command"
+  $command
+}
+
+stop_dhis_server() { local command=$1
+  debug "Stop DHIS2 server: $command"
+  $command
+}
+
+run_psql() {
+  sudo -u postgres psql "$@"
+}
+
+request_analytics() { local server_url=$1
+  # https://docs.dhis2.org/2.26/en/developer/html/webapi_generating_resource_analytics_tables.html
+  debug "Running analytics: $server_url"
+  curl -sS -X POST "$server_url/api/resourceTables/analytics"
+  echo
+}
+
+import_database() { local db_filename=$1 db_name=$2
+  debug "Recreate database: $db_name"
+  run_psql <<< "
+    DROP DATABASE IF EXISTS $db_name;
+    CREATE DATABASE $db_name OWNER 'dhis' ENCODING 'utf8';
+  "
+  debug "Import DB dump: $db_filename"
+  zcat -f  "$db_filename" | run_psql "$db_name"
+}
+
+install_dhis_war() { local warfile=$1 war_destination=$2
+  debug "Copy WAR: $warfile -> $war_destination"
+  cp "$warfile" "$war_destination"
+}
+
+run_analytics() {
+  local url=${1:-}
+  if ! test "$url"; then
+    die "Needs SERVER_URL"
+  else
+    request_analytics "$url"
+  fi
+}
+
+download_from_fileurl_or_repository() { local destdir=$1 db_source=$2 hard_update=$3 profile=$4
+  local protocol empty server org project blob branch path ourbranch github_dir
+  debug "DB origin URL: $db_source"
+
+  if echo "$db_source" | grep -q "^https://github.com/[^/]*/[^/]*/blob/"; then # github repo file
+    IFS="/" read protocol empty server org project blob branch path <<< "$db_source"
+    debug "DB origin is a github blob URL, clone repository"
+    github_dir="$destdir/$org/$project"
+    mkdir -p "$destdir/$org"
+    if ! test -e "$github_dir"; then
+      git clone "https://github.com/$org/$project" "$github_dir" >&2
+    fi
+    cd "$github_dir"
+
+    # Keep different branches (dhis2-installer-PROFILE) to avoid mixing soft/hard updates
+    # of profiles that use the same github URL
+    git checkout -f $branch >&2
+    ourbranch="dhis2-installer-$profile"
+    debug "Use branch: $ourbranch"
+    git branch "$ourbranch" >&2 || true
+    git checkout -f "$ourbranch" >&2
+
+    if test "$hard_update"; then
+      debug "Fetch newest changes from upstream repo: $path"
+      git fetch >&2
+      git merge $branch >&2
+      git diff-index --quiet HEAD || git commit -m "Hard update" >&2
+    fi
+    echo "$github_dir/$path"
+  else
+    download "$destdir" "$db_source"
+  fi
+}
+
+get_configuration_from_file() { local global_array=$1 profile=$2
+  local config_path="$HOME/.dhis2-installer.conf"
+  debug "Configuration file: $config_path"
+
+  if ! test -e "$config_path"; then
+    debug "Configuration file not found, use only command options"
+  else
+    debug "Configuration file found, get config from sections [global] and [profile:$profile]"
+    parse_ini_section "$global_array" "$config_path" "global"
+    parse_ini_section "$global_array" "$config_path" "profile:$profile"
+  fi
+}
+
+# Show stdout and stderr to console as usual but also append to the logs file
+config_logs() { local _logsdir=$1
+  local timestamp
+  logsdir=${_logsdir%%/}
+  mkdir -p "$logsdir"
+  debug "Logs directory: $logsdir"
+  timestamp=$(date +%Y-%m-%d_%H-%M-%S)
+  # Process substitution is not available in POSIX mode, disable it temporally
+  set +o posix
+  # This exec command redirect stderr/stdout to a file
+  exec &> >(tee -a "$logsdir/dhis2-installer-$timestamp.log")
+  # Now re-enable POSIX mode
+  set -o posix
+}
+
+wait_for_server() { local url=$1 max_wait=$2
+  local ts_start
+  debug "Wait for server ($max_wait secs max): $url"
+  ts_start=$(date +%s)
+
+  while true; do
+    if curl -s -o /dev/null --fail "$url"; then
+      debug "Server is running"
+      return 0
+    elif test $(expr $(date +%s) - $ts_start) -ge $max_wait; then
+      debug "Server could not be accessed"
+      return 1
+    else
+      sleep 1
+    fi
+  done
+}
+
+set_data_directory() { local directory=$1
+  local datadir=${directory%%/}
+  mkdir -p "$datadir"
+  debug "Data directory: $datadir"
+  echo "$datadir"
+}
+
+load_args_for_update_command() { local profile_or_first_option=${1:-}
+  local required_opts
+  local options=(
+    "soft" "hard"
+    "data-directory:" "logs-directory:"
+    "db-name:|n:" "db-source:"
+    "start-command:" "stop-command:"
+    "war-source:" "war-destination:"
+    "run-analytics"
+  )
+
+  if test "${profile_or_first_option}" && test "${profile_or_first_option:0:1}" != "-"; then
+    profile="$1"
+    shift 1
+    debug "Using profile: $profile"
+    get_configuration_from_file "args" "$profile"
+  fi
+  parse_args "args" "${options[*]}" print_help "$@"
+
+  # Validate arguments
+  if test "${args[hard]-}"; then  # hard update
+    required_opts=("db-name" "db-source" "start-command" "stop-command" "war-source" "war-destination")
+  else # soft update
+    required_opts=("db-name" "db-source" "start-command" "stop-command")
+  fi
+
+  for opt in ${required_opts[*]}; do
+    test "${args[$opt]-}" ||  die "Required option not found: --$opt"
+  done
+
+  echo "$profile"
+}
+
+update() {
+  local dbfile warfile datadir
+
+  load_args_for_update_command "$@"
+
+  config_logs "${args[logs-directory]-./logs}"
+  datadir=$(set_data_directory ${args[data-directory]-./data})
+
+  stop_dhis_server "${args[stop-command]}"
+
+  dbfile=$(download_from_fileurl_or_repository \
+    "$datadir" "${args[db-source]}" "${args[hard]-}" "${profile:-noprofile}")
+  import_database "$dbfile" "${args[db-name]}"
+
+  if test "${args[hard]-}"; then
+    warfile=$(download "$datadir" "${args[war-source]}")
+    install_dhis_war "$warfile" "${args[war-destination]}"
+  fi
+
+  start_dhis_server "${args[start-command]}"
+
+  if test "${args[run-analytics]-}"; then
+    wait_for_server "${args[run-analytics]}" 300
+    request_analytics "${args[run-analytics]}"
+  fi
+
+  debug "Done"
+}
+
+main() {
+  set -o posix
+  local command=${1:-}
+  test "$#" -ge 1 && shift 1
+
+  case "$command" in
+    "-h"|"--help")
+      print_help;;
+    "update")
+      update "$@";;
+    "run-analytics")
+      run_analytics "$@";;
+    "")
+      print_help;;
+    *)
+      die "Unknown command <$command>";;
+  esac
+}
+
+print_help() {
+  tabs 2
+  stderr "usage: dhis_installer [-h] <command> [<command_options>]"
+  stderr
+  stderr "Commands:"
+  stderr
+  stderr "\t" "update [PROFILE] [OPTIONS]" "\t\t" "Update existing DHIS2 instance"
+  stderr "\t" "run-analytics SERVER_URL" "\t\t" "Run analytics"
+  stderr "\t" "-h | --help" "\t\t" "Show this help message"
+  stderr
+  stderr "<update> options:"
+  stderr
+  stderr "\t" "--soft" "\t\t" "Drop current DB and install a fresh one (keeping existing DHIS2 war) [default]"
+  stderr "\t" "--hard" "\t\t" "Drop current DB and install a fresh one and update DHIS war"
+  stderr
+  stderr "\t" "--data-directory=DIRECTORY" "\t" "Directory to store downloaded files and repos"
+  stderr "\t" "--logs-directory=DIRECTORY" "\t" "Directory to store logs"
+  stderr
+  stderr "\t" "--db-name=NAME" "\t" "Database name"
+  stderr "\t" "--db-source=URL" "\t" "File URL or github URL (repo will be cloned)"
+  stderr
+  stderr "\t" "--start-command=NAME" "\t" "Command to start DHIS2 server"
+  stderr "\t" "--stop-command=NAME" "\t" "Command to stop DHIS2 server"
+  stderr
+  stderr "\t" "--war-source=URL" "\t" "URL of the DHIS2 WAR to install (used only on --hard)"
+  stderr "\t" "--war-destination=DIRECTORY" "\t" "Directory to save DHIS2 war (used only on --hard)"
+  stderr
+  stderr "\t" "--run-analytics=DHIS_SERVER_BASEURL" "\t" "Run Analytics in this DHIS2 server after update"
+}
+
+main "$@"
