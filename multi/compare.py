1bdfe981329093aa986c76f22c41a41c022add7a
Jordi <jordi@eyeseetea.com>
2018-03-10 16:08:51 +0100

    compare.py: can compare files only readable for root

--- multi/compare.py
+++ multi/compare.py
@@ -4,6 +4,7 @@
 Compare files between hosts (that we can directly access with ssh).
 """
 
+import sys
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter as fmt
 from tempfile import NamedTemporaryFile
 import subprocess as sp
@@ -18,8 +19,8 @@ def main():
 
     with named_temp(args.hosts[0]) as tmp0:
         with named_temp(args.hosts[1]) as tmp1:
-            fetch(args.hosts[0], args.path, tmp0.name)
-            fetch(args.hosts[1], args.path, tmp1.name)
+            fetch(args.hosts[0], args.path, tmp0.name, args.sudo)
+            fetch(args.hosts[1], args.path, tmp1.name, args.sudo)
             sp.call(args.cmpprog.split() + [tmp0.name, tmp1.name])
 
 
@@ -30,13 +31,23 @@ def parse_arguments():
         help='full path to the file we want to compare in the remote systems')
     add('--hosts', nargs=2, metavar='HOST', default=['arquimedes', 'behring'],
         help='remote hosts with the files we want to compare')
+    add('--sudo', action='store_true', help='if need sudo to access the files')
     add('--cmpprog', default='meld', help='program used to compare the files')
     add('--keep', action='store_true', help='keep the temporary copied files')
     return parser.parse_args()
 
 
-def fetch(host, remotename, localname):
-    sp.call(['scp', '%s:%s' % (host, remotename), localname])
+def fetch(host, remotename, localname, sudo=False):
+    try:
+        if not sudo:
+            sp.check_output(['scp', '%s:%s' % (host, remotename), localname])
+        else:
+            command = ('sudo cat %s' % remotename).encode('utf8')
+            out = sp.check_output(['ssh', host, '/bin/sh'], stderr=sp.STDOUT,
+                                  input=command).decode('utf8')
+            open(localname, 'wt').write(out)
+    except sp.CalledProcessError as e:
+        sys.exit('%s\nMaybe try with/without the "--sudo" argument?' % e)
 
 
 
