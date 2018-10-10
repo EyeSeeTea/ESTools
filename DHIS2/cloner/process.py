bb83ab73fe94fa149adc38a1266ec76406dd2ead
Jordi <jordi.burguet.castell@gmail.com>
2018-10-11 01:40:50 +0200

    new semantics
        "postprocess": [
            {
                "usernames": ["clone_test_user"]
            },
            {
                "usernames": ["test.dataentry"],
                "fromgroups": ["analytics training"],
                "addRoles": ["Run analytics"],
                "addRolesFromTemplate: "example_user"
            }

--- DHIS2/cloner/process.py
+++ DHIS2/cloner/process.py
@@ -14,29 +14,88 @@ Example:
 from __future__ import print_function
 
 import sys
+import time
+import requests
+
+import dhis2api
 
 
-class ProcessError(Exception):
-    pass
+
+def postprocess(cfg, entries):
+    """Add roles to the appropriate users as specified in entries.
+
+    The entries structure looks like:
+        [
+            {
+                "usernames": ["test.dataentry"],
+                "fromGroups": ["program1", "program2"],
+                "addRoles": ["role1", "role2"],
+                "addRolesFromTemplate": "validator.template"
+            }
+        ]
+    """
+    api = dhis2api.Dhis2Api(cfg['url'], cfg['username'], cfg['password'])
+
+    wait_for_server(api)
+
+    for entry in entries:
+        get = lambda x: entry.get(x, [])
+        add_roles(api, get('usernames'), get('fromGroups'),
+                  get('addRoles'), get('addRolesFromTemplate'))
 
 
-def debug_users(msg, users):
-    unames = ', '.join([user['userCredentials']['username'] for user in users])
-    debug('%s: %s' % (msg, unames or 'None'))
+def wait_for_server(api, delay=30, timeout=300):
+    debug('Check active API: %s' % api.api_url)
+    time.sleep(delay)  # in case tomcat is still starting
+    start_time = time.time()
+    while True:
+        try:
+            api.get('/me')
+            break
+        except requests.exceptions.ConnectionError:
+            if time.time() - start_time > timeout:
+                raise RuntimeError('Timeout: could not connect to the API')
+            time.sleep(1)
 
 
-def get_users_by_group_names(api, user_group_names):
-    debug('Get userGroups: name=%s' % ', '.join(user_group_names))
-    response = api.get('/userGroups', {
-        'paging': False,
-        'filter': 'name:in:[%s]' % ','.join(user_group_names),
-        'fields': ('id,name,'
-                   'users[:all,userCredentials[:all,userRoles[id,name]]]')})
-    return flatten(user_group['users'] for user_group in response['userGroups'])
+def add_roles(api, usernames, users_from_group_names,
+              rolenames, template_with_roles):
+    """
+    Take the users with the given usernames and all users in groups
+    users_from_group_names, enable them and add them roles from
+    add_roles and the roles in user add_roles_from_template.
+    """
+    users = unique(get_users_by_usernames(api, usernames) +
+                   get_users_by_group_names(api, users_from_group_names))
+    debug('Users to modify: %s' % ', '.join(get_username(x) for x in users))
+
+    roles_to_add = get_user_roles_by_name(api, rolenames)
+    if template_with_roles:
+        template = get_users_by_usernames(api, [template_with_roles])[0]
+        roles_to_add += get_roles(template)
+
+    for user in users:
+        roles = unique(get_roles(user) + roles_to_add)
+        user['userCredentials']['userRoles'] = roles
+        user['userCredentials']['disabled'] = False
+        api.put('/users/' + user['id'], user)
+
+
+def get_username(user):
+    return user['userCredentials']['username']
+
+
+def get_roles(user):
+    return user['userCredentials']['userRoles']
 
 
 def get_users_by_usernames(api, usernames):
-    debug('Get users: username=%s' % ', '.join(usernames))
+    "Return list of users corresponding to the given usernames"
+    debug('Get users: usernames=%s' % usernames)
+
+    if not usernames:
+        return []
+
     response = api.get('/users', {
         'paging': False,
         'filter': 'userCredentials.username:in:[%s]' % ','.join(usernames),
@@ -44,8 +103,23 @@ def get_users_by_usernames(api, usernames):
     return response['users']
 
 
+def get_users_by_group_names(api, user_group_names):
+    "Return list of users belonging to any of the given user groups"
+    debug('Get users from groups: names=%s' % user_group_names)
+
+    if not user_group_names:
+        return []
+
+    response = api.get('/userGroups', {
+        'paging': False,
+        'filter': 'name:in:[%s]' % ','.join(user_group_names),
+        'fields': ('id,name,'
+                   'users[:all,userCredentials[:all,userRoles[id,name]]]')})
+    return sum((x['users'] for x in response['userGroups']), [])
+
+
 def get_user_roles_by_name(api, user_role_names):
-    debug('Get user roles: name=%s' % ', '.join(user_role_names))
+    debug('Get user roles: name=%s' % user_role_names)
     response = api.get('/userRoles', {
         'paging': False,
         'filter': 'name:in:[%s]' % ','.join(user_role_names),
@@ -53,97 +127,17 @@ def get_user_roles_by_name(api, user_role_names):
     return response['userRoles']
 
 
-# Public interface.
-
-def enable_users(api, usernames=None, user_group_names=None):
-    "Enable Dhis2 users by settings user.userCredentials.disabled = false"
-    users_from_user_groups = (get_users_by_group_names(api, user_group_names)
-                              if user_group_names else [])
-    users_from_usernames = (get_users_by_usernames(api, usernames)
-                            if usernames else [])
-    users = list(unique(users_from_user_groups + users_from_usernames,
-                        lambda user: user['id']))
-    debug_users('Users to modify', users)
-
-    for user in users:
-        payload = {'id': user['id'], 'userCredentials': {'disabled': False}}
-        debug('Update user[username=%s]: %s' %
-              (user['userCredentials']['username'], payload))
-        api.patch('/users/' + user['id'], payload)
-
-    return users
-
-
-def add_user_roles(api, entries):
-    """
-    Add user roles to all users within userGroups from userTemplate and
-    extraUserRoles.
-
-    Config structure example:
-
-        [
-            {
-                'userGroups': ['_DATASET_M and E Officer'],
-                'userTemplate': 'district',
-                'extraUserRoles': ['role1', 'role2']
-            }
-        ]
-    """
-    for entry in entries:
-        user_groups_names = entry['userGroups']
-        user_template_username = entry['userTemplate']
-        extra_user_role_names = entry.get('extraUserRoles', [])
-
-        user_templates = get_users_by_usernames(api, [user_template_username])
-        if not user_templates:
-            raise ProcessError('No user template found: ' + user_template_username)
-        user_template = user_templates[0]
-        users_in_user_groups = get_users_by_group_names(api, user_groups_names)
-        user_roles_in_user_template = user_template['userCredentials']['userRoles']
-        extra_user_roles = get_user_roles_by_name(api, extra_user_role_names)
-
-        for user in users_in_user_groups:
-            existing_user_roles = user['userCredentials']['userRoles']
-            all_user_roles = flatten([
-                existing_user_roles,
-                user_roles_in_user_template,
-                extra_user_roles])
-            unique_user_roles = list(unique(all_user_roles,
-                                            lambda user_role: user_role['id']))
-            user_roles_pathload = [{'id': x['id'], 'name': x['name']}
-                                   for x in unique_user_roles]
-            user['userCredentials']['userRoles'] = user_roles_pathload
-            debug('Update user[username=%s]: setting %s user roles \n%s' %
-                  (user['userCredentials']['username'],
-                   len(user['userCredentials']['userRoles']),
-                   '\n'.join('  ' + ur['name']
-                             for ur in user['userCredentials']['userRoles'])))
-            api.put('/users/' + user['id'], user)
-
-
-def flatten(xss):
-    "Return flattened list of lists one level deep"
-    return [x for xs in xss for x in xs]
-
-
-def merge(d1, d2):
-    "Return dictionary with the contents of d1 and d2 (shared keys from d2)"
-    d3 = d1.copy()
-    d3.update(d2)
-    return d3
-
-
-def debug(*args, **kwargs):
-    "Print debug info to stderr"
-    print(*args, file=sys.stderr, **kwargs)
-
-
-def unique(sequence, tagger=None):
-    "Return iterable of unique elements in sequence"
-    tagger = tagger or (lambda x: x)
+def unique(xs):
+    "Return list of unique elements in xs, based on their x['id'] value"
+    xs_unique = []
     seen = set()
-    for item in sequence:
-        tag = tagger(item)
-        if tag not in seen:
-            seen.add(tag)
-            yield item
+    for x in xs:
+        if x['id'] not in seen:
+            seen.add(x['id'])
+            xs_unique.append(x)
+    return xs_unique
+
+
+def debug(txt):
+    print(txt)
+    sys.stdout.flush()
