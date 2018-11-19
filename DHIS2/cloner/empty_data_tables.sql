0cde3f37d9c0bad19e42a074c53a4f700d78bb55
Jordi <jordi.burguet.castell@gmail.com>
2018-11-19 15:54:38 +0100

    Use external sql file to delete the data from a dhis2 instance
    This would keep only the metadata, as we would want in a training server for example.

---
+++ DHIS2/cloner/empty_data_tables.sql
@@ -0,0 +1,18 @@
+-- Example sql script that would empty the tables in a dhis2 database
+-- that actually contain the data (as opposed to only metadata).
+
+-- You can run it as part of the cloning process by passing this file
+-- as argument --post-sql in dhis2_clone.
+
+DELETE FROM trackedentitydatavalueaudit;
+DELETE FROM trackedentitydatavalue;
+DELETE FROM datavalueaudit;
+DELETE FROM datavalue;
+DELETE FROM programstageinstance_messageconversation;
+DELETE FROM programstageinstancecomments;
+DELETE FROM programstageinstance;
+DELETE FROM trackedentityattributevalue;
+DELETE FROM programinstancecomments;
+DELETE FROM programinstance;
+DELETE FROM trackedentityattributevalueaudit;
+DELETE FROM trackedentityinstance;
