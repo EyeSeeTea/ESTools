8291efe966012c252166a106e520b4ce9cce583b
ifoche <nacho.foche@gmail.com>
2018-12-13 14:40:09 +0100

    add arguments and options to change public permissions, include objects and exclude objects

--- DHIS2/metadata_manipulation/change_user_groups.py
+++ DHIS2/metadata_manipulation/change_user_groups.py
@@ -1,5 +1,11 @@
 #/usr/bin/env python3
+
+"""
+Change sharing settings on a json file
+"""
+
 import json
+import argparse
 
 dhis_objects = [
     "indicators",
@@ -16,20 +22,42 @@ dhis_objects = [
     "trackedEntityTypes"
 ]
 
-for dhis_object in dhis_objects:
-    dhis_object_old = json.load(open('input.json')).get(dhis_object)
 
-    if not dhis_object_old:
-        continue
+def main():
+    args = get_args()
 
-    userGroupAccesses_new = json.load(open('userGroupAccesses.json'))['userGroupAccesses']
+    dhis_objects = [item for item in args.include if item not in args.exclude]
+    output = {}
+
+    for dhis_object in dhis_objects:
+        dhis_object_old = json.load(open('input.json')).get(dhis_object)
+
+        if not dhis_object_old:
+            print('WARN: Ignoring not found object %s' % dhis_object)
+            continue
+
+        userGroupAccesses_new = json.load(open('userGroupAccesses.json'))['userGroupAccesses']
+
+        elements_new = []
+        for element in dhis_object_old:
+            # Remove public access
+            element['publicAccess'] = args.public_access
+            element['userGroupAccesses'] = userGroupAccesses_new
+            elements_new.append(element)
+
+        output[dhis_object] = elements_new
+
+    json.dump(output, open('output.json', 'wt'), indent=2)
 
 
-    elements_new = []
-    for element in dhis_object_old:
-        # Remove public access
-        element['publicAccess'] = '--------'
-        element['userGroupAccesses'] = userGroupAccesses_new
-        elements_new.append(element)
+def get_args():
+    "Return arguments"
+    parser = argparse.ArgumentParser(description=__doc__)
+    add = parser.add_argument  # shortcut
+    add('--public-access', default='--------', help='set public permissions. Default: no public access (--------)')
+    add('--include', nargs='+', default=dhis_objects, help='DHIS2 objects to include. Default: All')
+    add('--exclude', nargs='+', default=[], help='DHIS2 objects to exclude. Default: None')
+    return parser.parse_args()
 
-    json.dump({dhis_object: elements_new}, open('results/%s.json' % dhis_object, 'wt'), indent=2)
+if __name__ == '__main__':
+    main()
