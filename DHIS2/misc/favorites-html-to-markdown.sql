e20d914ee3c78d90aa50e2381a83f6f89847e165
Arnau Sanchez <pyarnau@gmail.com>
2019-01-17 12:36:17 +0100

    Use temporal functions

--- DHIS2/misc/favorites-html-to-markdown.sql
+++ DHIS2/misc/favorites-html-to-markdown.sql
@@ -9,12 +9,12 @@
 --
 -- Example: $ cat favorites-html-to-markdown.sql | psql DBNAME
 
-CREATE OR REPLACE FUNCTION repl(source TEXT, _from TEXT, _to TEXT) RETURNS TEXT AS $$ BEGIN
+CREATE FUNCTION pg_temp.r(source TEXT, _from TEXT, _to TEXT) RETURNS TEXT AS $$ BEGIN
     RETURN REGEXP_REPLACE(source, _from, _to, 'gi');
 END $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION convert(source TEXT) RETURNS TEXT AS $$ BEGIN
-    RETURN repl(repl(repl(repl(repl(repl(repl(repl(source,
+CREATE FUNCTION pg_temp.convert(source TEXT) RETURNS TEXT AS $$ BEGIN
+    RETURN pg_temp.r(pg_temp.r(pg_temp.r(pg_temp.r(pg_temp.r(pg_temp.r(pg_temp.r(pg_temp.r(source,
         '</?i>', '_'),
         '</?b>', '*'),
         '</?strong>', '*'),
@@ -25,32 +25,33 @@ CREATE OR REPLACE FUNCTION convert(source TEXT) RETURNS TEXT AS $$ BEGIN
         '</ol>', '');
 END $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION update_database() RETURNS VOID AS $$ BEGIN
+CREATE FUNCTION pg_temp.update_database() RETURNS VOID AS $$ BEGIN
     -- Interpretation/comments text
-    UPDATE interpretation SET interpretationtext = convert(interpretationtext);
-    UPDATE interpretationcomment SET commenttext = convert(commenttext);
+    UPDATE interpretation SET interpretationtext = pg_temp.convert(interpretationtext);
+    UPDATE interpretationcomment SET commenttext = pg_temp.convert(commenttext);
 
     -- Favorites description
-    UPDATE eventreport SET description = convert(description);
-    UPDATE eventchart SET description = convert(description);
-    UPDATE chart SET description = convert(description);
-    UPDATE reporttable SET description = convert(description);
+    UPDATE eventreport SET description = pg_temp.convert(description);
+    UPDATE eventchart SET description = pg_temp.convert(description);
+    UPDATE chart SET description = pg_temp.convert(description);
+    UPDATE reporttable SET description = pg_temp.convert(description);
 END $$ LANGUAGE plpgsql;
 
 DO $$
     DECLARE
         input TEXT := CONCAT(
-            'Example: <br><li>item 1</li>, <ol>item2</ol>, <b>bold</b>, ',
+            'Example:<br><li>item1</li>, <ol>item2</ol>, <b>bold</b>, ',
             '<i>italic</i>, <strong>strong</strong><br>'
         );
-        expected_output TEXT := 'Example: \n- item 1, - item2, *bold*, _italic_, *strong*\n';
+        expected_output TEXT := 'Example:\n- item1, - item2, *bold*, _italic_, *strong*\n';
         result TEXT;
     BEGIN
-        SELECT convert(input) INTO result;
+        SELECT pg_temp.convert(input) INTO result;
         IF result <> expected_output THEN
-            RAISE EXCEPTION E'Test error:\n  input    = %,\n  output   = %,\n  expected = %', input, result, expected_output;
+            RAISE EXCEPTION E'Test error:\n  input    = %,\n  output   = %,\n  expected = %',
+                input, result, expected_output;
         ELSE
-            PERFORM update_database();
+            PERFORM pg_temp.update_database();
         END IF;
     END
 $$ LANGUAGE plpgsql;
