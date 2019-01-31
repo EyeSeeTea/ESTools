c6442064956ddc21a8841964b56c3120b01a8ee1
idelcano <ignaciodelcano@gmail.com>
2019-01-31 22:17:24 +0000

    update script to use a simple PL/pgSQL script

--- DHIS2/cloner/drop_view_tables.sql
+++ DHIS2/cloner/drop_view_tables.sql
@@ -1,41 +1,15 @@
--- Sql script to create the getTableViewNames() and dropViewTables() psql functions
--- and execute dropViewTables()
+-- Sql script to drop all the view tables
 
 -- You can run it as part of the cloning process by passing this file
 -- as argument --post-sql in dhis2_clone.
 
--- Create getTableViewNames function
--- Return a list of view tables propertly formatted
-CREATE FUNCTION getTableViewNames()
-RETURNS SETOF record
-LANGUAGE plpgsql as $$
+DO $$
 DECLARE
- rec record;
- BEGIN
-  FOR rec IN
-	SELECT CONCAT('_view_', REPLACE(LOWER(this_.name), ' ', '_'))
-	from sqlview this_ where this_.type = 'VIEW';
-  LOOP
-	return next rec;
-  END LOOP;
-END $$;
-
--- Create dropViewTables() function
-
-CREATE FUNCTION dropViewTables()
-RETURNS SETOF record
-LANGUAGE plpgsql as $$
-DECLARE
- rec record;
- BEGIN
-  FOR rec IN
-	(select * from getTableViewNames() as (names text))
-  LOOP
-	RAISE NOTICE 'DROP DATABASE %', rec;
-	'DROP DATABASE %', rec;
-  END LOOP;
-END $$;
-
--- Execute drop view tables
-
-SELECT dropViewTables();
+ rec RECORD;
+BEGIN
+	FOR rec IN SELECT CONCAT('_view_', REPLACE(LOWER(this_.name), ' ', '_')) as name from sqlview this_ where this_.type = 'VIEW'
+		LOOP
+			RAISE NOTICE 'DROP VIEW %', rec.name;
+			EXECUTE CONCAT('DROP VIEW IF EXISTS ', rec.name);
+		END LOOP;
+END; $$
