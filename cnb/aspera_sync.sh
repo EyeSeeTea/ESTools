8f4309c434e5f2ac0aa7e642884d782185f47911
idelcano <ignaciodelcano@gmail.com>
2019-03-20 10:48:32 +0100

    add "local" to local variables, rename variables, and refactor

--- cnb/aspera_sync.sh
+++ cnb/aspera_sync.sh
@@ -8,44 +8,48 @@ ROOT_LOCAL_FOLDER="$HOME/data"
 ROOT_REMOTE_FOLDER='/CNB/CNBPROYECTO1/CBNSHARE'
 
 syncPath(){
-        REMOTE_FOLDER=$1
-        LOCAL_DATA=$2
+        local remote_folder=$1
+        local local_data=$2
         #Get ls with formatted dates and ordered by time
-        lslist=$(TZ=utc ls $LOCAL_DATA -altr --color=auto --time-style=full-iso)
+        local local_files=$(TZ=utc ls $local_data -altr --color=auto \
+         --time-style=full-iso)
 
         #Get filenames column
-        localfilenames=$(echo "$lslist" | awk '{print $9}')
+        local local_filenames=$(echo "$local_files" | awk '{print $9}')
 
         #Get aspera folder info and remove all less files table
-        result=$(aspera shares browse -i -u $REMOTE_USER --host $REMOTE_HOST \
-         --path=$REMOTE_FOLDER --sort mtime_a |
-        sed -n  '/----/,/----/p' |sed '$d;1d;2d' )
+        local result=$(aspera shares browse -i -u $REMOTE_USER \
+        --host $REMOTE_HOST --path=$remote_folder --sort mtime_a \
+        | sed -n  '/----/,/----/p' |sed '$d;1d;2d' )
 
         echo "$result" |
-        while IFS= read -r asperarow;do
+        while IFS= read -r aspera_row;do
                 #Get aspera item values
-                filename=$(echo $asperarow | awk '{print $4}')
-                size=$(echo $asperarow | awk '{print $2}')
-                type=$(echo $asperarow | awk '{print $1}')
-                date=$(echo $asperarow | awk '{print $3}')
+                local filename=$(echo $aspera_row | awk '{print $4}')
+                local size=$(echo $aspera_row | awk '{print $2}')
+                local type=$(echo $aspera_row | awk '{print $1}')
+                local date=$(echo $aspera_row | awk '{print $3}')
 
                 #Get and convert ls and aspera dates to timestamp
-                lsfilerow=$(echo "$lslist" | sed -n '/'"$filename"'$/p')
-                if ! [ -z "$lsfilerow" ];then
-                        localdate=$(echo $lsfilerow | awk '{print $6}')
-                        localhour=$(echo $lsfilerow | awk '{print $7}')
-                        localdate=$(echo $localdate$localhour | cut -f1 -d".")
-                        localfiledate=$(busybox date -u -d $localdate -D \
-                         %Y-%m-%d%H:%M:%S)
-                        localfiledate=$(date -d "$localfiledate" +%s)
+                local local_file=$(echo "$local_files" \
+                | sed -n '/'"$filename"'$/p')
+                if ! [ -z "$local_file" ];then
+                        #format ls date to timestamp
+                        local local_date=$(echo $local_file | awk '{print $6}')
+                        local local_hour=$(echo $local_file | awk '{print $7}')
+                        local_date=$(echo $local_date$local_hour | \
+                        cut -f1 -d".")
+                        local local_file_timestamp=$(busybox date -u \
+                        -d $local_date +%s -D  %Y-%m-%d%H:%M:%S)
                 fi
-                filedate=$(busybox date -u -d $date -D %Y-%m-%dT%H:%M:%SZ)
-                filedate=$(date -d "$filedate" +%s)
+                #format aspera item date to timestamp
+                local file_timestamp=$(busybox date -u \
+                -d $date +%s -D %Y-%m-%dT%H:%M:%SZ)
 
                 #Check if the filename exist in the local filenames,
                 #and if the creation date is before than server updated date
-                if [[ "$localfilenames" == *"$filename"* ]] && \
-                [[ $filedate -lt $localfiledate ]];then
+                if [[ "$local_filenames" == *"$filename"* ]] && \
+                [[ $file_timestamp -lt local_file_timestamp ]];then
                         echo "Already exist: $filename"
                 #Ignore files with 0kb.
                 elif [ $size == 0 -a ${type:0:1} == "-" ]
@@ -54,8 +58,8 @@ syncPath(){
                 else
                         echo "Start download: $filename"
                         aspera shares download -i -u $REMOTE_USER \
-                        --host $REMOTE_HOST --source=$REMOTE_FOLDER/$filename \
-                         --destination=$LOCAL_DATA
+                        --host $REMOTE_HOST --source=$remote_folder/$filename \
+                         --destination=$local_data
                 fi
         done
 
