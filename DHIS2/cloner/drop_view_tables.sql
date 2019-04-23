a606e0e60fa13db5d6d941b7a214feac39af28da
idelcano <ignaciodelcano@gmail.com>
2019-04-23 10:02:34 +0200

    fix drop view table

--- DHIS2/cloner/drop_view_tables.sql
+++ DHIS2/cloner/drop_view_tables.sql
@@ -7,7 +7,7 @@ DO $$
 DECLARE
  rec RECORD;
 BEGIN
-	FOR rec IN SELECT CONCAT('_view_', REPLACE(LOWER(this_.name), ' ', '_')) as name from sqlview this_ where this_.type = 'VIEW'
+     	FOR rec IN SELECT CONCAT('_view_', REPLACE(REPLACE(LOWER(this_.name), ' ', '_'), '_-_', '_')) as name from sqlview this_ where this_.type = 'VIEW'
 		LOOP
 			RAISE NOTICE 'DROP VIEW %', rec.name;
 			EXECUTE CONCAT('DROP VIEW IF EXISTS ', rec.name);
