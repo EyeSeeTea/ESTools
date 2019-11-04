322cc3327b268e8c6da73dc1036334bd4d87779c
idelcano <ignaciodelcano@gmail.com>
2019-11-04 09:06:51 +0100

    added multiple dataset option

--- DHIS2/metadata_manipulation/dataset_metadata_finder.py
+++ DHIS2/metadata_manipulation/dataset_metadata_finder.py
@@ -8,42 +8,44 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter as fmt
 
 import json
 import dhis2 as d2
-dataset_query="dataSets/%s?fields=name,dataSetElements[dataElement[*]]"
+dataset_query = "dataSets/%s?fields=name,dataSetElements[dataElement[*]]"
+default_category_combo = "Xr12mI7VPn3"
 
 def main():
     args = get_args()
     d2.USER = args.user
     d2.URLBASE = args.urlbase
     rows = list()
-    dataset = d2.get(dataset_query % args.dataset)
-    for data_set_element in dataset["dataSetElements"]:
-        row = dict()
-        data_element = data_set_element["dataElement"]
-        row["name"] = data_element["name"]
-        if "code" in data_element.keys():
-            row["code"] = data_element["code"]
-        else:
-            row["code"] = "no code"
-        row["id"] = data_element["id"]
-        category_combo = d2.get_object(data_element["categoryCombo"]["id"])
-        row["topic"] = category_combo["name"]
-        row["category_combo_options"] = list()
-        for category_option_combo_id in category_combo["categoryOptionCombos"]:
-            category_option_combo = d2.get_object(category_option_combo_id["id"])
-            row["category_combo_options"].append({"name": category_option_combo["name"], "id": category_option_combo["id"]})
-        rows.append(row)
+    for dataset_uid in args.dataset:
+        dataset = d2.get(dataset_query % dataset_uid)
+        for data_set_element in dataset["dataSetElements"]:
+            row = dict()
+            data_element = data_set_element["dataElement"]
+            row["name"] = data_element["name"]
+            row["dataset"] = dataset["name"]
+            if "code" in data_element.keys():
+                row["code"] = data_element["code"]
+            else:
+                row["code"] = "no code"
+            row["id"] = data_element["id"]
+            category_combo = d2.get_object(data_element["categoryCombo"]["id"])
+            row["topic"] = category_combo["name"]
+            row["category_combo_options"] = list()
+            for category_option_combo_id in category_combo["categoryOptionCombos"]:
+                category_option_combo = d2.get_object(category_option_combo_id["id"])
+                row["category_combo_options"].append({"name": category_option_combo["name"], "id": category_option_combo["id"]})
+            rows.append(row)
 
 
     with open(args.output, mode='w') as file:
         csv_final_file = create_csv(file)
         for row in rows:
             for category_option_combo in row["category_combo_options"]:
-                print([dataset["name"], row["name"], row["id"], row["code"], row["topic"],
+                print([row["dataset"], row["name"], row["id"], row["code"], row["topic"],
+                       category_option_combo["name"], category_option_combo["id"],default_category_combo, ""])
+                csv_final_file.writerow([row["dataset"], row["name"], row["id"], row["code"], row["topic"],
                                          category_option_combo["name"], category_option_combo["id"],
-                                         "Xr12mI7VPn3", ""])
-                csv_final_file.writerow([dataset["name"], row["name"], row["id"], row["code"], row["topic"],
-                                         category_option_combo["name"], category_option_combo["id"],
-                                         "Xr12mI7VPn3", ""])
+                                         default_category_combo, ""])
 
 
 def create_csv(file):
@@ -56,7 +58,7 @@ def get_args():
     parser = ArgumentParser(description=__doc__, formatter_class=fmt)
     add = parser.add_argument  # shortcut
     add('-o', '--output', help='output file')
-    add('-d', '--dataset', help='dataset id')
+    add('-d', '--dataset', nargs='*', help='dataset id')
     add('-u', '--user', metavar='USER:PASSWORD', required=True,
         help='username and password for server authentication')
     add('--urlbase', default='https://extranet-uat.who.int/dhis2',
