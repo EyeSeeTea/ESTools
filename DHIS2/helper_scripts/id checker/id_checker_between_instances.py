075ef8f8b45fb82c40c5af2c273c79227976a43f
idelcano <ignaciodelcano@gmail.com>
2020-04-23 11:48:46 +0200

    add format

--- DHIS2/helper_scripts/id checker/id_checker_between_instances.py
+++ DHIS2/helper_scripts/id checker/id_checker_between_instances.py
@@ -1,8 +1,8 @@
 import json
 
-with open("microroles_prod.json") as json_file:
+with open("dev_roles_2.json") as json_file:
     all_orgunits = json.load(json_file)
-    with open("microroles_dev2.json") as json_file_2:
+    with open("2.30-micro-roles-1.0.11.json") as json_file_2:
         data = json.load(json_file_2)
         for dev in all_orgunits["userRoles"]:
             exist = False
@@ -10,4 +10,8 @@ with open("microroles_prod.json") as json_file:
                 if dev["id"] == prod["id"]:
                     exist = True
             if not exist:
-                print (dev)
+                print(dev['id'])
+                print (dev['name'])
+                if "description" in dev.keys():
+                    print(dev['description'])
+                print("--------------------------")
