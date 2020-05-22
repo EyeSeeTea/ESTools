64ca7dba2bc797a47f89c7a1e5617cfbe3729d88
idelcano <ignaciodelcano@gmail.com>
2020-05-22 11:36:08 +0200

    Added notification to csv

---
+++ DHIS2/metadata_sync_utils/notification_to_csv/README.md
@@ -0,0 +1,11 @@
+
+Utility to convert a list of notifications in json format into csv.
+
+The input format must be
+
+{
+ "messages":[
+		{notification},
+		{notification}
+             ]
+}
---
+++ DHIS2/metadata_sync_utils/notification_to_csv/notification_to_csv.py
@@ -0,0 +1,34 @@
+import json
+import csv
+import os
+from os import listdir
+from os.path import isfile, join
+input_folder = "input"
+output_folder = "output"
+is_json = lambda fname: os.path.splitext(fname)[-1] in ['.json']
+is_not_json = lambda fname: not os.path.splitext(fname)[-1] in ['.json']
+is_not_git = lambda fname: not fname.startswith(".git")
+applied_filter = is_not_git if is_json else is_not_json
+
+files = [f for f in filter(applied_filter, listdir(input_folder)) if isfile(join(input_folder, f))]
+for path_file in files:
+    with open(join(output_folder, path_file).replace(".json","")+".csv", mode='w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        with open(join(input_folder, path_file)) as json_file:
+            data = json.load(json_file)
+            for rule in data["messages"]:
+                csv_writer.writerow(['-----------------------------------------------'])
+                csv_writer.writerow(['imported', 'updated', 'ignored', 'total'])
+                imported = rule["stats"]["imported"]
+                updated = rule["stats"]["updated"]
+                ignored = rule["stats"]["ignored"]
+                total = rule["stats"]["imported"] + rule["stats"]["updated"] + rule["stats"]["ignored"]
+                csv_writer.writerow([imported, updated, ignored, total])
+                csv_writer.writerow(['uid', 'message', "", ""])
+                for messages in rule["report"]["messages"]:
+                    if "uid" in messages.keys():
+                        uid = messages["uid"]
+                    else:
+                        uid = None
+                    message = messages["message"]
+                    csv_writer.writerow([uid, message, "", ""])
