baf6498a643c257a251de656b0c56e0a6c50389a
Jordi BC <jordi.burguet.castell@eyeseetea.com>
2022-12-01 17:12:04 +0100

    multirun.py: several improvements.
    Change the names of certain arguments.
    
    Has "hosts.txt" as the default file with the names of the hosts to run the command on.
    
    Uses a more modern subprocess.run() instead of subprocess.check_output()
    
    Shows the stderr when a command fails, with its error code and stdout that can be seen apart.
    
    And some other cosmetic changes.

--- multi/multirun.py
+++ multi/multirun.py
@@ -13,65 +13,77 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter as fmt
 def main():
     try:
         args = get_arguments()
+
         nodes = get_nodes(args.hosts_file)
-        display = get_display(args.format, nodes, args.literal)
+
+        display = get_display(nodes, args.short, args.literal)
+
         for node, out in multirun(args.command, nodes, args.timeout):
-            display(node, out)
+            print(display(node, out))
+
     except (AssertionError, OSError) as e:
         sys.exit(e)
 
 
 def get_arguments():
     parser = ArgumentParser(description=__doc__, formatter_class=fmt)
+
     add = parser.add_argument  # shortcut
     add('command', help='command to run')
-    add('--hosts-file', default='multirun_hosts.txt',
-        help='file with hosts to run command on')
-    add('--timeout', type=int, default=10, help='number of seconds to wait')
-    add('--format', default='short', choices=['short', 'long'],
-        help='output format')
-    add('--literal', action='store_true', help='do not parse the response')
+    add('-f', '--hosts-file', default='hosts.txt', help='file with hosts')
+    add('-t', '--timeout', type=int, default=10, help='seconds to wait')
+    add('-s', '--short', action='store_true', help='short output format')
+    add('-l', '--literal', action='store_true', help='do not parse response')
+
     return parser.parse_args()
 
 
 def get_nodes(fname):
     "Return list of nodes read from fname"
     nodes = []
+
     for line in open(fname):
         if len(line.strip()) == 0 or line.lstrip().startswith('#'):
-            continue
+            continue  # so there can be comments and empty lines too
+
         parts = line.strip().split()
+
         assert len(parts) == 1 or parts[1].startswith('#'), \
             'Malformed line in %s: %r' % (fname, line)
+
         nodes.append(parts[0])
+
     return nodes
 
 
-def get_display(format_type, nodes, literal):
-    "Return a function that, given a node and its output, displays it prettily"
-    if format_type == 'short':
+def get_display(nodes, short, literal):
+    "Return a function that, given a node and its output, formats it prettily"
+    if short:
         fmt = '%%-%ds # %%s' % max(len(x) for x in nodes)
-        return lambda node, out: print(fmt % (node, out if literal else out.replace('\n', ' ')))
+        if literal:
+            return lambda node, out: fmt % (node, out)
+        else:
+            return lambda node, out: fmt % (node, out.replace('\n', ' '))
     else:
-        return lambda node, out: print('%s\n%s\n%s\n' %
-                                       (node, '=' * len(node), out))
+        return lambda node, out: f'==> {node} <==\n{out}\n'
 
 
 def multirun(command, nodes, timeout=10, max_workers=10):
     "Run command in nodes and yield their output, computed in parallel"
     def run(node):
-        out = sp.check_output(['ssh', node, '/bin/sh'], stderr=sp.STDOUT,
-                              input=command.encode('utf8'), timeout=timeout)
-        return out.decode('utf8').strip()
+        proc = sp.run(['ssh', node, '/bin/sh'], input=command.encode('utf8'),
+                      capture_output=True, timeout=timeout)
+        stderr = proc.stderr.decode('utf8').strip()
+        stdout = proc.stdout.decode('utf8').strip()
+        return (('' if proc.returncode == 0 else f'=> [{proc.returncode}]\n') +
+                ('' if not stderr else f'=> stderr:\n{stderr}\n=> stdout:\n') +
+                stdout)
 
     with ThreadPoolExecutor(max_workers) as executor:
         future_to_node = {executor.submit(run, node): node for node in nodes}
         for future in as_completed(future_to_node):
             node = future_to_node[future]
-            try:
-                yield node, future.result()
-            except Exception as e:
-                yield node, str(e)
+            yield node, future.result()
 
 
 
