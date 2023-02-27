1a77c0597d2004e87da3825261583bb8c107796c
inadcl <ignaciodelcano@gmail.com>
2023-02-27 10:58:24 +0100

    fixed value and move user creedentials to params

--- DHIS2/leish2dhis/converter.py
+++ DHIS2/leish2dhis/converter.py
@@ -1,6 +1,7 @@
 # Import the xlrd module
 import json
 import os
+import subprocess
 
 stop = "***** PLEASE DON'T MODIFY THESE HEADERS *****"
 sheetsnames = ["Population", "VL", "CL"]
@@ -16,8 +17,15 @@ def getPos(letra):
     return posicion - 1
 
 
+def fixValue(value):
+    if value in ["ENDEMIC","ENDEMICITY_DOUBTFUL","PREVIOUSLY_REPORTED_CASES","AT_RISK","NO_AUTHOCHTHONOUS_CASES_REPORTED]"]:
+        return ["ENDEMIC","ENDEMICITY_DOUBTFUL","PREVIOUSLY_REPORTED_CASES","AT_RISK","NO_AUTHOCHTHONOUS_CASES_REPORTED]"].index(value)+1
+
+    return value
+
+
 def getData(dataelement, programid, period, type, orgunitid, value):
-    if value is None:
+    if value is None or value == "":
         return None
     if type == "datasets":
         dhisvalue = {
@@ -26,7 +34,7 @@ def getData(dataelement, programid, period, type, orgunitid, value):
             "orgUnit": orgunitid,
             "categoryOptionCombo": "Xr12mI7VPn3",
             "attributeOptionCombo": "Xr12mI7VPn3",
-            "value": row[6],
+            "value": value,
             "storedBy": "leish2dhis script"}
     else:
         dhisvalue = {
@@ -38,7 +46,7 @@ def getData(dataelement, programid, period, type, orgunitid, value):
             "dataValues": [{
                 "storedBy": "leish2dhis script",
                 "dataElement": dataelement,
-                "value": value
+                "value": fixValue(value)
             }]}
     return dhisvalue
 
@@ -48,23 +56,38 @@ def getData(dataelement, programid, period, type, orgunitid, value):
 def getdate(period, pos):
     return str(period) + "-" + str(pos) + "-01T00:00:00.000"
 
-def checkOrgUnit(uid, row):
+def checkOrgUnit(uid, row, credentials, server):
     import requests
 
-    # Configurar las credenciales de autenticación básica
-    usuario = ''
-    contraseña = ''
-    credenciales = (usuario, contraseña)
-
     # URL del recurso que se desea verificar si existe
-    url_recurso = 'http://192.168.1.125:8080/api/organisationUnits/'+uid
+    url = server+'/api/organisationUnits/'+uid
 
     # Realizar la llamada a la API
-    respuesta = requests.get(url_recurso, auth=credenciales)
+    respuesta = requests.get(url, auth=credentials)
 
     # Verificar el resultado de la llamada
     if respuesta.status_code != 200:
         print('org unit: ' + uid + " error: " + str(respuesta.status_code) + "ou info: " + str(json.dumps(row)))
+
+
+def connect_parameters():
+    import argparse
+
+    # Create the parser object
+    parser = argparse.ArgumentParser(description='Connect to a server using provided user and password')
+
+    # Add the arguments
+    parser.add_argument('--user', help='Username for server authentication', required=True)
+    parser.add_argument('--password', help='Password for server authentication', required=True)
+    parser.add_argument('--server', help='Server name or IP address', required=True)
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Use the arguments to connect to the server
+    print(f"Connecting to server {args.server} as user {args.user}")
+    return args.user, args.password, args.server
+
 
 ruta_directorio = "input"
 archivos_xlsx = [archivo for archivo in os.listdir(ruta_directorio) if archivo.endswith(".xlsx")]
@@ -99,7 +122,6 @@ for archivo in archivos_xlsx:
                 valuecellsnumber = table[count][9]
 
                 count = count + 1
-                # mKpmhDgK4Cq(dataElements GEN_pop_Leish)	deKCGAGoEHz	dataSet Population data	2016	pMukkUmnnkh
 
                 for row in table[valuefirstrow:]:
                     orgunitid = row[0]
@@ -110,7 +132,7 @@ for archivo in archivos_xlsx:
                         fixed_period = period
                         if (type == "programs"):
                             fixed_period = period + "-01-01T00:00:00.000"
-                        dhis2value = getData(dataelement, programid, period, type, orgunitid, value)
+                        dhis2value = getData(dataelement, programid, fixed_period, type, orgunitid, value)
                     else:
                         for x in range(1, valuecellsnumber):
                             fixed_period = period
@@ -139,6 +161,10 @@ for archivo in archivos_xlsx:
         except:
             print("hoja no encontrada"+ sheetname)
             print("fichero" + ruta_archivo)
+
+user, password, server= connect_parameters()
+    # Configurar las credenciales de autenticación básica
+credentials = (user, password)
 for orgunitkey in allOrgUnits.keys():
-    checkOrgUnit(orgunitkey, allOrgUnits[orgunitkey])
+    checkOrgUnit(orgunitkey, allOrgUnits[orgunitkey], credentials, server)
     # n06IItYE45N dataelement VL_casees_by provenance_T	i5JSf4ffFl2	programs i5JSf4ffFl2	2016	pMukkUmnnkh
