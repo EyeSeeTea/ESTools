ecf169050c1a64f7ca6515dbfb1d85c592256740
idelcano <ignaciodelcano@gmail.com>
2023-03-08 15:21:44 +0100

    added mini scripts to help to split files and push todhis2

---
+++ DHIS2/leish2dhis/pushlist/pushfiles.py
@@ -0,0 +1,26 @@
+import os
+import requests
+import json
+
+# URL de destino para hacer la petición POST
+url = '/api/38/tracker?async=false'
+
+# Credenciales para autenticación básica
+username = ''
+password = ''
+
+# Obtener la lista de archivos .json del directorio actual
+lista_archivos = [f for f in os.listdir('.') if f.endswith('.json')]
+
+# Iterar sobre los archivos y hacer una petición POST con su contenido
+for archivo in lista_archivos:
+    # Abrir el archivo y cargar su contenido en un diccionario
+    with open(archivo, 'r') as f:
+        data = json.load(f)
+
+    # Hacer la petición POST con el contenido del archivo y las credenciales de autenticación
+    response = requests.post(url, json=data, auth=(username, password))
+
+    # Imprimir el resultado de la petición
+    print(f"Archivo {archivo}: {response.status_code} {response.text}")
+    print(f"continue")
---
+++ DHIS2/leish2dhis/splitbysize/spliter.py
@@ -0,0 +1,18 @@
+import json
+
+# Abrimos el archivo .json y cargamos los datos en una variable
+with open('normalprogram.json', 'r') as f:
+    data = json.load(f)["events"]
+    print(len(data))
+
+    # Obtenemos los valores del diccionario y los almacenamos en una lista
+    eventos =data
+
+    # Separamos los eventos en listas de 250 elementos
+    eventos_por_lista = [eventos[i:i + 2500] for i in range(0, len(eventos), 2500)]
+
+    # Guardamos cada lista de eventos en un archivo .json separado
+    for i, lista_eventos in enumerate(eventos_por_lista):
+        nombre_archivo = f"eventos_{i}.json"
+        with open(nombre_archivo, 'w') as f:
+            json.dump({"events": lista_eventos }, f)
