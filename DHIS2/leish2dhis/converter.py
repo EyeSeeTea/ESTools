bf8b67237d55340e80d7ecffde7162e816be56b4
idelcano <ignaciodelcano@gmail.com>
2023-03-08 15:18:41 +0100

    last changes to make the events work

--- DHIS2/leish2dhis/converter.py
+++ DHIS2/leish2dhis/converter.py
@@ -2,11 +2,14 @@
 import json
 import os
 import subprocess
+import requests
 
 stop = "***** PLEASE DON'T MODIFY THESE HEADERS *****"
 sheetsnames = ["Population", "VL", "CL"]
 endemic_optionset = ["ENDEMIC", "ENDEMICITY_DOUBTFUL", "PREVIOUSLY_REPORTED_CASES", "AT_RISK",
-                     "NO_AUTHOCHTHONOUS_CASES_REPORTED]"]
+                     "NO_AUTHOCHTHONOUS_CASES_REPORTED"]
+progamstage_query = "/api/programs/%s.json?fields=programStages"
+program_programstages = dict()
 fixOrgUnits = dict()
 fixOrgUnits["vGW4iEyxLOv"] = "N4ymCL5RSIn"
 fixOrgUnits["HqKgvQhHoy3"] = "bbFCXdo5j9T"
@@ -18,6 +21,19 @@ fixOrgUnits["zlHkJUx9qql"] = "GLV6lqi0B0O"
 fixOrgUnits["C81JPWWymfa"] = "B3zC2a6SXU8"
 
 allOrgUnits = dict()
+
+
+def getProgramStage(programid):
+    user, password, server = connect_parameters()
+    # Configurar las credenciales de autenticación básica
+    credentials = (user, password)
+    respuesta = requests.get(server + progamstage_query %  programid, auth=credentials)
+    programstageid = json.loads(respuesta.text)["programStages"][0]["id"]
+    # Verificar el resultado de la llamada
+    if respuesta.status_code != 200:
+        print('org unit: ' + programid + " error: " + str(respuesta.status_code) + "ou info: " + str(json.dumps(row)))
+    return programstageid
+
 
 def get_code():
     command = "java CodeGenerator"
@@ -36,8 +52,8 @@ def getPos(letra):
 
 
 def fixValue(value):
-    if value in endemic_optionset:
-        return endemic_optionset.index(value) + 1
+    if str(value).upper().replace(" ","") in endemic_optionset:
+        return endemic_optionset.index(str(value).upper().replace(" ","")) + 1
     else:
         return value
 
@@ -57,27 +73,36 @@ def getData(dataelement, programid, period, type, orgunitid, value):
             "value": value,
             "storedBy": "leish2dhis script"}
     else:
+        if programid not in program_programstages.keys():
+            program_programstages[programid] = getProgramStage(programid)
         dhisvalue = {
-            "programType": "WITHOUT_REGISTRATION",
-            "orgUnit": orgunitid,
-            "program": programid,
-            "event": get_code(),
-            "status": "ACTIVE",
-            "eventDate": period,
-            "dataValues": [{
-                "storedBy": "leish2dhis script",
-                "dataElement": dataelement,
-                "value": fixValue(value)
-            }]}
+                "programType": "WITHOUT_REGISTRATION",
+                "orgUnit": orgunitid,
+                "program": programid,
+                "programStage": program_programstages[programid],
+                "event": get_code(),
+                "status": "ACTIVE",
+                "eventDate": period,
+                "occurredAt": period,
+                "createdAt": period,
+                "dataValues": [{
+                    "storedBy": "leish2dhis script",
+                    "dataElement": dataelement,
+                    "occurredAt": period,
+                    "createdAt": period,
+                    "value": fixValue(value)
+                }]}
     return dhisvalue
 
 
 def getdate(period, pos):
-    return str(period) + "-" + str(pos) + "-01T00:00:00.000"
+    if pos<10:
+        return str(period) + "-0" + str(pos) + "-01T00:00:00.000"
+    else:
+        return str(period) + "-" + str(pos) + "-01T00:00:00.000"
 
 
 def checkOrgUnit(uid, row, credentials, server):
-    import requests
 
     # URL del recurso que se desea verificar si existe
     url = server + '/api/organisationUnits/' + uid
@@ -113,7 +138,9 @@ ruta_directorio = "input"
 archivos_xlsx = [archivo for archivo in os.listdir(ruta_directorio) if archivo.endswith(".xlsx")]
 
 import openpyxl
-
+totalDataValues= []
+totalNormalProgram = []
+totalAnormalProgram = []
 for archivo in archivos_xlsx:
     ruta_archivo = os.path.join(ruta_directorio, archivo)
 
@@ -121,9 +148,12 @@ for archivo in archivos_xlsx:
     print(ruta_archivo)
     active_workbook = obj.active
     for sheetname in obj.sheetnames:
+        sheetname = str(sheetname)
         print(sheetname)
+        if sheetname == "Dictionary":
+            continue
         try:
-            sheet = obj.get_sheet_by_name(sheetname)
+            sheet = obj[sheetname]
             table = [[cell.value for cell in row] for row in sheet.rows]
 
             period = ""
@@ -132,10 +162,13 @@ for archivo in archivos_xlsx:
             programid = ""
             count = 0
             dhis2data = []
+            dhis2dataanormalevent = []
             while table[count][0] != stop:
                 period = table[count][5]
                 dataelement = table[count][2]
                 type = table[count][4].lower()
+                if type not in ["datasets", "programs"]:
+                    print("type error; " +type)
                 programid = table[count][3]
                 letterStartPos = getPos(table[count][8])
                 valuefirstrow = table[count][7]
@@ -143,7 +176,7 @@ for archivo in archivos_xlsx:
 
                 count = count + 1
 
-                for row in table[valuefirstrow:]:
+                for row in table[valuefirstrow-1:]:
                     orgunitid = row[0]
 
                     allOrgUnits[row[0]] = str(json.dumps(row)) + " filename: " + ruta_archivo
@@ -153,24 +186,30 @@ for archivo in archivos_xlsx:
                         if (type == "programs"):
                             fixed_period = period + "-01-01T00:00:00.000"
                         dhis2value = getData(dataelement, programid, fixed_period, type, orgunitid, value)
+                        if dhis2value is not None:
+                            if type == "programs":
+                                dhis2dataanormalevent.append(dhis2value)
+                                totalAnormalProgram.append(dhis2value)
+                            else:
+                                dhis2data.append(dhis2value)
+                                totalDataValues.append((dhis2value))
                     else:
-                        for x in range(1, valuecellsnumber):
+                        for x in range(1, valuecellsnumber+1):
                             fixed_period = period
                             value = row[x + letterStartPos - 1]
                             if type == "programs":
                                 fixed_period = getdate(period, x)
                             dhis2value = getData(dataelement, programid, fixed_period, type, orgunitid, value)
-                    if dhis2value is not None:
-                        dhis2data.append(dhis2value)
-
+                            if dhis2value is not None:
+                                dhis2data.append(dhis2value)
+                                totalNormalProgram.append(dhis2value)
+                resultanomalprogram = ""
                 result = ""
                 if type == "datasets":
                     result = {"dataValues": dhis2data}
                 else:
-                    if "events" in result:
-                        result = {"events": result["events"].appdhis2data}
-                    else:
-                        result = {"events": dhis2data}
+                    result = {"events": dhis2data}
+                    resultanomalprogram = {"events": dhis2dataanormalevent}
 
                 # Serializing json
                 json_object = json.dumps(result, indent=4)
@@ -178,13 +217,46 @@ for archivo in archivos_xlsx:
                 # Writing to sample.json
                 with open("output/" + archivo.replace(".xlsx", "") + sheetname + ".json", "w") as outfile:
                     outfile.write(json_object)
-        except:
+
+                if len(dhis2dataanormalevent) > 0:
+                    json_object = json.dumps(resultanomalprogram, indent=4)
+
+                    # Writing to sample.json
+                    with open("output/curl" + archivo.replace(".xlsx", "") + sheetname + ".json", "w") as outfile:
+                        outfile.write(json_object)
+        except Exception as e:
+            print('Failed : '+ str(e))
             print("hoja no encontrada" + sheetname)
-            print("fichero" + ruta_archivo)
+            print("fichejoero" + ruta_archivo)
+
+json_object = json.dumps({"events":totalAnormalProgram}, indent=4)
+
+# Writing to sample.json
+with open("output/curl" + "rareprogram"+ ".json", "w") as outfile:
+    outfile.write(json_object)
+
+
+json_object = json.dumps({"events":totalNormalProgram}, indent=4)
+
+# Writing to sample.json
+with open("output/" + "normalprogram"+ ".json", "w") as outfile:
+    outfile.write(json_object)
+
+
+json_object = json.dumps({"dataValues":totalDataValues}, indent=4)
+
+# Writing to sample.json
+with open("output/" + "datavalues"+ ".json", "w") as outfile:
+    outfile.write(json_object)
+
+
 
 user, password, server = connect_parameters()
 # Configurar las credenciales de autenticación básica
 credentials = (user, password)
+
+
 for orgunitkey in allOrgUnits.keys():
-    checkOrgUnit(orgunitkey, allOrgUnits[orgunitkey], credentials, server)
+    if orgunitkey not in fixOrgUnits:
+        checkOrgUnit(orgunitkey, allOrgUnits[orgunitkey], credentials, server)
     # n06IItYE45N dataelement VL_casees_by provenance_T	i5JSf4ffFl2	programs i5JSf4ffFl2	2016	pMukkUmnnkh
