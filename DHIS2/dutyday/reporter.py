57af11b7358a99d7501ccde868fcaad4f601368a
idelcano <ignaciodelcano@gmail.com>
2024-02-27 20:48:44 +0100

    Added new features, update, multiple outputs, validation..

--- DHIS2/dutyday/reporter.py
+++ DHIS2/dutyday/reporter.py
@@ -5,16 +5,68 @@ import subprocess
 import json
 
 import paramiko
+import argparse
+
+hostdetails = dict()
+report_details = {}
+
+def validate_host(host):
+    validate(host, "server_name")
+    validate(host, "url")
+    validate(host, "user")
+    validate(host, "host")
+
+
+def validate(item, key):
+    if key not in item.keys():
+        print(f"Key {key} not found in item {item}.")
+        sys.exit(1)
+    else:
+        return item[key]
+
+
+def local_update(config):
+    branch = validate(config, "branch")
+    url = validate(config, "url")
+    try:
+        subprocess.check_call(['python', 'githubupdater.py', url, branch])
+    except subprocess.CalledProcessError as e:
+        print(f"Error al ejecutar repo_updater.py: {e}")
+
+
+def remote_update(host, url, branch):
+    print("\n"+execute_command_on_remote_machine(host, host.logger_path + "logger.sh githubupdater " + url + " " + branch))
+
+
+def validate_config(config_file):
+    servers = validate(config_file, "servers")
+    for server in servers:
+        validate_host(server)
+    actions = validate(config_file, "actions")
+    for action in actions:
+        validate(action, "type")
+        validate(action, "description")
+        validate(action, "servers")
+    config = validate(config_file, "config")
+    validate(config, "url")
+    validate(config, "branch")
+    print("Config file is valid.")
+
+
+def update_scripts(data):
+    local_update(data.config.url, data.config.branch)
+    update_servers(data)
 
 
 #this script will be executed on local docker with vpn active
+
 def execute_command_on_remote_machine(host, command):
-    path_to_private_key = '/root/.ssh/id_rsa'
+    path_to_private_key = host.keyfile
     private_key = paramiko.RSAKey.from_private_key_file(path_to_private_key)
 
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(host, username="tomcatuser", pkey=private_key)
+    client.connect(host.hostname, username=host.username, pkey=private_key)
 
     stdin, stdout, stderr = client.exec_command(command)
     output = stdout.read().decode().strip()
@@ -24,95 +76,173 @@ def execute_command_on_remote_machine(host, command):
     return output
 
 
-def ask_clone(host, file):
-    result = execute_command_on_remote_machine(host, "/home/tomcatuser/bin/logger.sh clonelogger "+file)
+def run_action(host, action):
+    validate(host, action.type)
+    if action.type == "cloning":
+        return analyze_clone(host)
+    elif action.type == "monit":
+        return analyze_monit(host)
+    elif action.type == "backups":
+        return analyze_db(host)
+    elif action.type == "analytics":
+        return analyze_analytics(host)
+    elif action.type == "custom":
+        return analyze_custom_script(host, action.command)
+
+
+def remote_update(host, url, branch):
+    result = execute_command_on_remote_machine(host, host.logger_path + "logger.sh githubupdater ", url, branch)
     return result
 
 
-def ask_monit(host):
-    result = execute_command_on_remote_machine(host, "/home/tomcatuser/bin/logger.sh monitlogger")
+def analyze_clone(host):
+    validate(host, "cloning")
+    result = execute_command_on_remote_machine(host, host.logger_path + "logger.sh clonelogger "+host.cloning)
     return result
 
 
-def ask_db(host, file):
-    result = execute_command_on_remote_machine(host, "/home/tomcatuser/bin/logger.sh databaselogger "+file)
+def analyze_monit(host):
+    result = execute_command_on_remote_machine(host, host.logger_path + "logger.sh monitlogger")
     return result
 
 
-def ask_space(host, command):
+def analyze_db(host):
+    validate(host, "backups")
+    result = execute_command_on_remote_machine(host, host.logger_path + "logger.sh databaselogger "+host.backups)
+    return result
+
+
+def analyze_custom_script(host, command):
     result = execute_command_on_remote_machine(host, command)
     return result
 
 
-def ask_analytics(host, logfile, server, type):
+def analyze_analytics(host, logfile, server, type):
     if type == "docker":
         server = server + ""
-        analyticslog = execute_command_on_remote_machine(host, "/home/tomcatuser/bin/logger.sh analyticslogger docker " + server + " " +logfile)
+        analyticslog = execute_command_on_remote_machine(host, host.logger_path + "logger.sh analyticslogger docker " + server + " " +logfile)
         return analyticslog
     elif type == "tomcat":
-        analyticslog = execute_command_on_remote_machine(host, "/home/tomcatuser/bin/logger.sh analyticslogger tomcat " + server + " " +logfile)
+        analyticslog = execute_command_on_remote_machine(host, host.logger_path + "logger.sh analyticslogger tomcat " + server + " " +logfile)
 
         return analyticslog
 
     print('Informe generado.')
 
-
-def runlogger(file):
-    logger = ""
-    with open(file, 'r') as file:
-        data = json.load(file)
-        for item in data:
-            logger = logger + "\n"
-            logger = logger + "Type: {"+item['type']+"}, Server: {"+item['server']+"}"
-            logger = logger + "\n"
-            url = item.get('url')
-            if url:
-                logger = logger + "URL: "+url
-            logger = logger + "\n"
-            if "backups" in item.keys():
-                backups = item.get('backups')
-                logger = logger + "\n"
-                logger = logger + "Backups: "+ backups
-                logger = logger + "\n"
-                logger = logger + ask_db(item.get("host"), item.get("backups"))
-                logger = logger + "\n"
-
-            if "analytics" in item.keys():
-                analytics = item.get('analytics')
-                logger = logger + "\n"
-                logger = logger + "Catalina: "+analytics
-                logger = logger + "\n"
-                logger = logger + ask_analytics(item.get("host"), item.get("analytics"), item.get("server"), item.get("type"))
-                logger = logger + "\n"
-            type = item.get('type')
-            if type == "monit":
-                monit = item.get('monit')
-                logger = logger + "\n"
-                logger = logger + "Monit: "+monit
-                logger = logger + "\n"
-                logger = logger + ask_monit(item.get("host"))
-                logger = logger + "\n"
-
-            if "cloning" in item.keys():
-                logger = logger + "\n"
-                logger = logger + "Cloning: " + item.get("host")
-                logger = logger + "\n"
-                logger = logger + ask_clone(item.get("host"), item.get("cloning"))
-                logger = logger + "\n"
+def add_to_report(server, action, result, description):
+    if server not in report_details.keys():
+        report_details.append({server: {}})
+    report_details[server].append({action: {"result": result, "description": description}})
 
 
-            if type == "diskspace":
-                logger = logger + "\n"
-                logger = logger + "Disk Space: " + item.get("host")
-                logger = logger + "\n"
-                logger = logger + ask_space(item.get("host"), item.get("command"))
-                logger = logger + "\n"
+def load_host(server):
+    server_name = server.get('server_name')
+    type = server.get('type')
+    tomcat = server.get('tomcat_folder')
+    docker = server.get('docker_name')
+    logger_path = server.get("loggerpath")
+    host = server.get('host')
+    url = server.get('url')
+    user = server.get('user')
+    backups = server.get('backups')
+    keyfile = server.get('keyfile')
+    analytics = server.get('analytics')
+    cloning = server.get('cloning')
+    temp_dict = {
+        "server_name": server_name,
+        "type": type,
+        "tomcat": tomcat,
+        "docker": docker,
+        "host": host,
+        "url": url,
+        "user": user,
+        "backups": backups,
+        "keyfile": keyfile,
+        "loggerpath": logger_path,
+        "analytics": analytics,
+        "cloning": cloning
+    }
+    hostdetails[server_name] = {k: v for k, v in temp_dict.items() if v is not None}
 
-            print('------')
-            logger = logger + "\n"
-            logger = logger + "\n"
-    print(logger)
+
+def load_servers(data):
+    for server in data["servers"]:
+        load_host(server)
+        validate_host(hostdetails[server.get("server_name")])
+
+
+def update_servers(data):
+    for item in data["actions"]:
+        if "github_update" == item.get("type"):
+            for server in item.get("servers"):
+                branch = validate(data["config"], "branch")
+                url = validate(config["config"], "url")
+                host = hostdetails[server]
+                remote_update(host, url, branch)
+
+
+def run_logger(data):
+        for item in data["actions"]:
+            if "backups" == item.get("type"):
+                for server in item.get("servers"):
+                    result = run_action(hostdetails[server], "backups")
+                    add_to_report(server, "backups", result, item.get('description'))                    
+
+            if "analytics" == item.get("type"):
+                for server in item.get("servers"):
+                    result = run_action(hostdetails[server], "analytics")
+                    add_to_report(server, "analytics", result, item.get('description'))
+
+            if "cloning" == item.get("type"):
+                for server in item.get("servers"):
+                    result = run_action(hostdetails[server], "cloning")
+                    add_to_report(server, "cloning", result, item.get('description'))
+
+            if "custom" == item.get("type"):
+                for server in item.get("servers"):
+                    result = run_action(hostdetails[server], "custom")
+                    add_to_report(server, "custom", result, item.get('description'))
 
 
 if __name__ == '__main__':
-    runlogger('config.json')
+    parser = argparse.ArgumentParser(description="Arguments --file: config file, --check check config file")
+    
+    parser.add_argument('--file', type=str, required=True, help='Path to the config file.')
+    parser.add_argument('--check', action='store_true', help='Mode in to check the config file. Not required. ')
+    parser.add_argument('--update', type=bool, help='Update report and logger files ', default=False)
+    parser.add_argument('--mode', type=str, help='Report mode print/printandpush/json/html ', default="print")
+    
+    args = parser.parse_args()
+
+    with open(args.file, 'r') as file:
+        config = json.load(file)
+        if args.check:
+            validate_config(config)
+        elif args.update:
+            update_scripts(config)
+        else:
+            result = run_logger(config)
+            if args.mode == "json":
+                print(json.dumps(result))
+            elif args.mode == "print":
+                for server in result.keys():
+                    print("\n\n\n------------------------------------------------")
+                    print("------------------------------------------------")
+                    print("\n"+server+"\n")
+                    for action in result[server].keys():
+                        print("------------------------Start_action-----------------------")
+                        print(action.get("description")+"\n")
+                        print(action.get("result")+"\n")
+                        print("------------------------END-----------------------")
+                    print("------------------------------------------------")
+                    print("------------------------------------------------")
+            
+            elif args.mode == "html":
+                print("<html><head><title>Report</title></head><body>")
+                for server in result.keys():
+                    print("<div class='instance'> <div class='server' <h1>" + server + "</h1>"+ "</div>")
+                    for action in result[server].keys():
+                        print("<div class='description' <p>" + action.get("description") + "</p>"+ "</div>")
+                        print("<div class='result' <p>" + action.get("result") + "</p>"+ "</div>")
+                    print("</div>")
+                print("</body></html>")
