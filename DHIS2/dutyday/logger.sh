53cae064b5028b3297a3ae60fbc318012bd99a61
nshandra <34254522+nshandra@users.noreply.github.com>
2024-03-22 12:44:56 +0100

    fix: apply shellcheck suggestions and formatting to logger.sh

--- DHIS2/dutyday/logger.sh
+++ DHIS2/dutyday/logger.sh
@@ -3,8 +3,8 @@
 set +x
 
 test_connection() {
-  local servername=$1
-  echo "$servername connected"
+    local servername=$1
+    echo "$servername connected"
 }
 
 githubupdater() {
@@ -14,57 +14,54 @@ githubupdater() {
     export http_proxy=$3
     export https_proxy=$3
     echo "Running githubupdater: file: $file branch: $branch command=$command"
-    python3 $command $file $branch
+    python3 "$command" "$file" "$branch"
     echo "Github update finished"
 }
 
 monitlogger() {
-	sudo monit status | awk '/Process/{p=$0} /Not monitored/{print p "\n" $0 "\n"}'
+    sudo monit status | awk '/Process/{p=$0} /Not monitored/{print p "\n" $0 "\n"}'
 }
-
 
 databaselogger() {
     local file=$1
-    tail -10 $file 2>&1
+    tail -10 "$file" 2>&1
 }
-
 
 catalinaerrors() {
     local file=$1
-    cat $file | grep "ERROR" | grep -v "Unable to render velocity template"
+    grep "ERROR" "$file" | grep -v "Unable to render velocity template"
 }
 
-clonelogger(){
+clonelogger() {
     local file=$1
     START_DATE=$(date -d 'last Saturday' '+%Y-%m-%d')
-    echo $START_DATE
-    awk "/$START_DATE/{flag=1} flag" $file | grep  "ERROR\|error\|FAIL\|Error\|OK\|$START_DATE" | sed  "s/'[^:]*:[^']*'/USER:PASSWORDHIDDEN/g "
+    echo "$START_DATE"
+    awk "/$START_DATE/{flag=1} flag" "$file" | grep "ERROR\|error\|FAIL\|Error\|OK\|$START_DATE" | sed "s/'[^:]*:[^']*'/USER:PASSWORDHIDDEN/g "
 }
-
 
 analyticslogger() {
     local type=$1
     local file=$2
     local host=$3
-    
+
     LOG_FILE=$file
-    if [ $type == "docker" ];then
-      dockerid=$(docker ps | grep $host"-core" | awk '{print $1}')
-      if [[ -n $dockerid ]]; then
-        response=$(docker cp "${dockerid}:${file}" /tmp/)
-        file=$(basename $file)
-        LOG_FILE="/tmp/$file"
-      else
-        echo "docker id not found"
-        exit 1
-      fi
+    if [ "$type" == "docker" ]; then
+        dockerid=$(docker ps | grep "$host""-core" | awk '{print $1}')
+        if [[ -n $dockerid ]]; then
+            docker cp "${dockerid}:${file}" /tmp/
+            file=$(basename "$file")
+            LOG_FILE="/tmp/$file"
+        else
+            echo "docker id not found"
+            exit 1
+        fi
     fi
 
     START_DATE=$(date -d"-2 days" +%Y-%m-%d)+"|"+$(date -d"-1 days" +%Y-%m-%d)+"|"+$(date -d"-0 days" +%Y-%m-%d)
 
-    START_LINE=$(grep -E "$START_DATE" $LOG_FILE | grep 'Table update start: analytics, earliest:'| grep 'last years=500')
-    END_LINE=$(grep -E "$START_DATE" $LOG_FILE | grep 'Analytics tables updated' | grep -v ' 00:0')
-    ERROR_LINES=$(grep -E "$START_DATE" $LOG_FILE | grep 'ERROR')
+    START_LINE=$(grep -E "$START_DATE" "$LOG_FILE" | grep 'Table update start: analytics, earliest:' | grep 'last years=500')
+    END_LINE=$(grep -E "$START_DATE" "$LOG_FILE" | grep 'Analytics tables updated' | grep -v ' 00:0')
+    ERROR_LINES=$(grep -E "$START_DATE" "$LOG_FILE" | grep 'ERROR')
     printf "%s" "$START_LINE$END_LINE$ERROR_LINES" | awk '{gsub("T"," ",$3); print}' | sort -k3,3 -k4,4
 }
 # Script starts here
@@ -77,30 +74,30 @@ fi
 command=$1
 shift
 case $command in
-    monitlogger)
-        monitlogger $@
-        ;;
-    analyticslogger)
-        analyticslogger $@
-        ;;
-    databaselogger)
-        databaselogger $@
-        ;;
-    clonelogger)
-        clonelogger $@
-        ;;
-    githubupdater)
-        githubupdater $@
-        ;;
-    test_connection)
-        test_connection $@
-        ;;
-    catalinaerrors)
-        catalinaerrors $@
-        ;;
+monitlogger)
+    monitlogger "$@"
+    ;;
+analyticslogger)
+    analyticslogger "$@"
+    ;;
+databaselogger)
+    databaselogger "$@"
+    ;;
+clonelogger)
+    clonelogger "$@"
+    ;;
+githubupdater)
+    githubupdater "$@"
+    ;;
+test_connection)
+    test_connection "$@"
+    ;;
+catalinaerrors)
+    catalinaerrors "$@"
+    ;;
 
-    *)
-        echo "Command not found: $command"
-        exit 1
-        ;;
+*)
+    echo "Command not found: $command"
+    exit 1
+    ;;
 esac
