6432c8febd7dce3097f2167fcd76bd0e6bb4d67a
milorad <milorad@eyeseetea.com>
2024-10-18 16:12:03 +0200

    update time format to hour decimal

--- reporting/clickup_to_dhis2/utils/unixtime.ts
+++ reporting/clickup_to_dhis2/utils/unixtime.ts
@@ -11,10 +11,6 @@ export function getUnixTimeForLastNDays(n: number): { startUnixTime: number, end
 }
 
 export function formatDuration(ms: number): string {
-    const totalSeconds = Math.floor(ms / 1000);
-    const hours = Math.floor(totalSeconds / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-    const seconds = totalSeconds % 60;
-  
-    return `${hours}h ${minutes}m ${seconds}s`;
+  const hours = ms / 3600000;
+  return hours.toFixed(2);
 }
